ad71136e4966fbbb71d3a40a80d2bc2ce8c5332e
subarnarajshrestha1993 <153310137+subarnarajshrestha1993@users.noreply.github.com>
2023-12-08 11:00:40 -0600

    carRentalChallenge
    commiting code for car rental challenge

---
+++ GetCarRentalInfo.py
@@ -0,0 +1,73 @@
+
+
+import json
+import datetime
+
+
+
+
+def read_file_data():
+
+    file_path = input("Enter the file path:")
+    fp = open(file_path, 'r')
+    file_data = json.load(fp)
+    return file_data
+
+
+def aggregate_data(file_data):
+    aggregated_data = list()
+    for record in file_data:
+        id = record['id']
+        event_Type = record['type']
+        timestamp = record.get('timestamp')
+        comments = record.get('comments')
+        ids = [item['id'] for item in aggregated_data]
+        if id  in ids:
+            # append the other keys
+            for new_record in aggregated_data:
+                if id == new_record['id']:
+                    new_record[event_Type.lower() + "_" + "timestamp"] = timestamp
+                    new_record[event_Type.lower() + "_" + "comments"] = comments
+        else:
+            # insert the keys
+            new_record = dict()
+            new_record['id'] = id
+            new_record[event_Type.lower()+"_"+"timestamp"] = timestamp
+            new_record[event_Type.lower() + "_" + "comments"] = comments
+            aggregated_data.append(new_record)
+    print(aggregated_data)
+    return aggregated_data
+
+
+
+def print_stats(aggregated_data):
+    for record in aggregated_data:
+        if record.get('end_timestamp') and 'start_timestamp' in record.keys():
+            session_duration = (int(record['start_timestamp'] )- int(record.get('end_timestamp')))/(60*60)
+            if session_duration > 24:
+                record['over_time'] = True
+            else:
+                record['over_time'] = False
+        else:
+            record['over_time'] = False
+        if record.get('end_comments'):
+            record['car_damaged'] = True
+        else:
+            record['car_damaged'] = False
+
+
+    json_string = json.dumps(aggregated_data, indent=4)
+    with open('output_file.json', 'w') as fp:
+        fp.write(json_string)
+
+def main():
+    file_data = read_file_data()
+    aggregated_data = aggregate_data(file_data)
+    print_stats(aggregated_data)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.txt
@@ -0,0 +1,20 @@
+Instructions to Use the Code:
+
+1. The script expects the input json file to be provided as an input. Full path of the input file has to be provided as input.
+   eg: Enter the file path:sample_output.json
+2. The output of the script is being saved into a json file (output.json), in the same folder where script is present
+   The output json file will have
+   a) id
+   b) start_timestamp
+   c) end_timestamp
+   d) start_comments
+   f) end_comments
+   g) over_time (if the car was rented for more than 4 hours)
+   h) car_damaged (if end event has comment)
+
+
+
+
+ The script can be run for larger data file as well.
+
+
